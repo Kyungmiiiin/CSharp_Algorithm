11e928e461aea6a3f5b5bd06a3b5e287981b0d87
경민 박 <kyung_min2@naver.com>
2024-01-11 15:48:45 +0900

    2024.01.11 과제

---
+++ 01.List/Class1.cs
@@ -0,0 +1,26 @@
+﻿
+namespace List_과제
+{
+    public class Program
+    {
+        static void Main()
+        {
+            List<int> list = new List<int>();
+
+            Console.Write("숫자를 입력하세요 : ");
+            int i = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < i; j++)
+            {
+                list.Add(j);
+            }
+
+            list.Remove(0);
+
+            for (int j = 0; j < list.Count; j++)
+            {
+                Console.WriteLine($"{list[j]}번 데이터");
+            }
+        }
+    }
+}
