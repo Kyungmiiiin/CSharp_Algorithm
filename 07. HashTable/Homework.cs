9f3774f670243936e9bd05b98553cdd7cc928e6b
경민 박 <kyung_min2@naver.com>
2024-01-18 17:14:54 +0900

    치트키 과제

---
+++ 07. HashTable/Homework.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._HashTable
+{
+    public class CheatKey
+    {
+        private Dictionary<string, Action> cheatDic;
+
+         public CheatKey()
+         {  
+            cheatDic = new Dictionary<string, Action>();
+
+            cheatDic.Add("Money", ShowMeTheMoney);
+            cheatDic.Add("Level", ThereIsNoCowLevel);
+         }
+        public void Run( string cheatKey )
+        {
+            cheatDic.TryGetValue(cheatKey, out Action action);
+            action?.Invoke();
+        }
+
+        public void ShowMeTheMoney()
+        {
+             Console.WriteLine("골드를 늘려주는 치트키 발동 ! ");
+        }
+        public void ThereIsNoCowLevel()
+        {
+             Console.WriteLine("바로 승리합니다 치트키 발동 ! ");
+        }
+
+        static void Main()
+        {
+            CheatKey cheatKey = new CheatKey();
+            
+            string input = Console.ReadLine();
+            cheatKey.Run(input);
+        }
+
+    }
+}
